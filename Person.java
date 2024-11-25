0f09ff4bc7b95af8040d69704f5cc02bfe312219
TrishaAnneMacalindong <23-11136@g.batstate-u.edu.ph>
2024-11-25 15:37:09 +0800

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,27 @@
+class Person {
+    protected String name;
+    protected String address;
+
+    
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
